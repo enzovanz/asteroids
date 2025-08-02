9ce7ccb467968fc58cd77870aa07c05c80e7e827
enzovanz <enzovanzuita@gmail.com>
2025-08-02 02:31:29 +0200

    Window creation

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.get_init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
